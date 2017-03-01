4f71a66f06a5ce613096c7270057f5bc642740e1
Kian Davoudi-Rad <kdr@Kians-MacBook-Pro.local>
2017-02-28 17:49:15 -0800

    added UIButton for Button Delegate method

---
+++ QRColourfull/QRColourfull/UIButtonProtocol.h
@@ -0,0 +1,21 @@
+//
+//  UIButtonsProtocol.h
+//  QRColourfull
+//
+//  Created by Kian Davoudi-Rad on 2017-02-28.
+//  Copyright © 2017 Kian Davoudi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+// Protocol definition starts here
+@protocol UIButtonProtocol <NSObject>
+
+    // Required methods
+    @required
+    // Optional methods
+    @optional
+    
+
+@end
